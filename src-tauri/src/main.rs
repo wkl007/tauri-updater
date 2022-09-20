8a0b4796a655d5c4a21faf5602044dd50a8bea0d
wkl007 <499657357@qq.com>
2022-09-20 10:04:07 +0800

    feat: 配置系统托盘

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,7 @@ all(not(debug_assertions), target_os = "windows"),
 windows_subsystem = "windows"
 )]
 
-use tauri::{Manager};
+use tauri::{Manager, SystemTray, SystemTrayMenu, CustomMenuItem};
 
 #[derive(Clone, serde::Serialize)]
 struct Payload {
@@ -11,11 +11,22 @@ struct Payload {
   cwd: String,
 }
 
+fn system_tray() -> SystemTray {
+  let open = CustomMenuItem::new("open".to_string(), "open");
+  let quit = CustomMenuItem::new("quit".to_string(), "quit");
+  let tray_menu = SystemTrayMenu::new()
+    .add_item(open)
+    .add_item(quit);
+  let tray = SystemTray::new().with_menu(tray_menu);
+  return tray;
+}
+
 fn main() {
   tauri::Builder::default()
     .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
       app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap();
     }))
+    .system_tray(system_tray())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
